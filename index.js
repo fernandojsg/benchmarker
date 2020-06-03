af665309fdfc8a0240e78e49231362126228e79e
Fernando Serrano <fernandojsg@gmail.com>
2020-06-03 22:45:24 +0200

    Added getReport and groups

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import Stats from "incremental-stats-lite";
 
 const DEFAULT_GLOBAL_OPTIONS = {
   verbose: false,
-  summary: true
+  summary: true,
+  iterations: 10
 };
 
 const log = console.log;
@@ -14,9 +15,15 @@ const DEFAULT_OPTIONS = {
 export class Benchmarks {
   constructor(globalOptions, benchDefaultOptions) {
     this.benchs = [];
+    this.benchsByGroup = {
+      nogroup: []
+    };
+    this.currentGroup = this.benchsByGroup.nogroup;
+
     this.options = Object.assign(DEFAULT_GLOBAL_OPTIONS, globalOptions);
     this.benchDefaultOptions = Object.assign(DEFAULT_OPTIONS, benchDefaultOptions);
   }
+
   add(bench) {
     if (!bench.options) {
       bench.options = this.benchDefaultOptions;
@@ -24,13 +31,72 @@ export class Benchmarks {
       bench.options = Object.assign(this.benchDefaultOptions, bench.options);
     }
 
+    if (!bench.iterations) {
+      bench.iterations = this.options.iterations;
+    }
+
     this.benchs.push(bench);
+    this.currentGroup.push(bench);
+
+    return this;
+  }
+
+  getReport(format = "json") {
+    let groups = [];
+    let json = {
+      groups: groups,
+      meanAll: 0,
+      sumAll: 0
+    };
+
+    Object.entries(this.benchsByGroup).forEach(([groupName, benchmarks]) => {
+      if (benchmarks.length === 0) {
+        return;
+      }
+
+      let group = {
+        groupName: groupName,
+        benchmarks: [],
+        meanAll: 0,
+        sumAll: 0
+      };
+
+      groups.push(group);
+
+      benchmarks.forEach(bench => {
+        const stats = bench.stats.getAll();
+        let current = {
+          name: bench.name,
+          stats: {}
+        };
+        group.benchmarks.push(current);
+        Object.entries(stats).forEach(([key, value]) => {
+          current.stats[key] = value;
+        });
+
+        group.meanAll += stats.mean;
+        group.sumAll += stats.sum;
+      });
+
+      json.meanAll += group.meanAll;
+      json.sumAll += group.sumAll;
+    });
+    return json;
+  }
+
+  group(name) {
+    if (!this.benchsByGroup[name]) {
+      this.benchsByGroup[name] = [];
+    }
+
+    this.currentGroup = this.benchsByGroup[name];
+
     return this;
   }
 
   run() {
     this.benchs.forEach(bench => {
-      let stats = new Stats();
+      bench.stats = new Stats();
       let context = {};
       if (bench.prepareGlobal) {
         bench.prepareGlobal(context);
@@ -44,7 +110,7 @@ export class Benchmarks {
         let t0 = Date.now();
         bench.execute(context);
         let total = Date.now() - t0;
-        stats.update(total);
+        bench.stats.update(total);
 
         // @todo Logging options
         if (this.options.verbose) {
@@ -67,7 +133,7 @@ export class Benchmarks {
         log(
           `${"-".repeat(len)}\n${title}\n${"-".repeat(len)}`
         );
-        const values = stats.getAll();
+        const values = bench.stats.getAll();
         Object.entries(values).forEach(([key, value]) => {
           if (key !== "n") {
             console.log(`- ${key}: ${value.toFixed(2)}`);
